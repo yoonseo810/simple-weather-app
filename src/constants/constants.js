72d927111e062aeefd33fe24fd1cbc2aef1838b7
Yoonseong <yoonseo1989@outlook.com>
2024-04-20 21:19:59 -0400

    configuring env variable

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,2 @@
-const API_KEY = '0651101b1fbde1f693e8b5e044b36973';
-
 export const getUrl = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
