72a91f82f059b17762f524e75ff5b312b91344a9
Yoonseong <yoonseo1989@outlook.com>
2024-04-20 21:26:27 -0400

    save

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,4 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const getUrl = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
